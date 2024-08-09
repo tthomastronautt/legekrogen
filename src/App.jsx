c6862d0697f1fe0bab216e5bfa4a7bd65b7c2aac
SerhiiIkin <messileonl@gmail.com>
2024-08-09 10:00:28 +0200

    add instal axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import viteLogo from "@assets/react.svg";
 
 function App() {
+    const postSubscribers = async () => {
+        const data = axiosInstance
+            .post("subscribe", {
+                name: "Anna",
+                email: "anna2@gmail.com",
+                message: "test",
+            })
+            .then((res) => res.data);
+    };
+
     return (
-      <>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <>
+            <img src={viteLogo} className="logo" alt="Vite logo" />
         </>
     );
 }
