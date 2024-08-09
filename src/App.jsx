a036ecb45c93dfaa3560b46c9e54bd5f2f2d0c59
SerhiiIkin <messileonl@gmail.com>
2024-08-09 10:50:47 +0200

    skabelon

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-import viteLogo from "@assets/react.svg";
-
-function App() {
-    const postSubscribers = async () => {
-        const data = axiosInstance
-            .post("subscribe", {
-                name: "Anna",
-                email: "anna2@gmail.com",
-                message: "test",
-            })
-            .then((res) => res.data);
-    };
-
-    return (
-        <>
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-        </>
-    );
-}
+const App = () => <></>;
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
             "@components": "/src/components",
             "@pages": "/src/pages",
             "@routes": "/src/routes",
+            "@utils": "/src/utils",
         },
     },
     plugins: [react()],
