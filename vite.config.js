f28edaab223fda507f7e1b34ca0fb04505d692dc
SerhiiIkin <messileonl@gmail.com>
2024-08-09 09:45:30 +0200

    add folders

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@assets': '/src/assets',
-      '@components': '/src/components',
-      '@pages': '/src/pages',
-      '@routes': '/src/routes',
-      '@utils': '/src/utils',
-      '@variables': '/src/variables',
-      '@providers': '/src/providers',
+    resolve: {
+        alias: {
+            "@": "/src",
+            "@assets": "/src/assets",
+            "@components": "/src/components",
+            "@pages": "/src/pages",
+            "@routes": "/src/routes",
+        },
     },
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+});
