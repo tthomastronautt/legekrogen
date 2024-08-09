c6fbb7b2363a4b8bab93b25a98fe957da4aaa0b8
SerhiiIkin <messileonl@gmail.com>
2024-08-09 09:43:29 +0200

    add alias

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@routes': '/src/routes',
+      '@utils': '/src/utils',
+      '@variables': '/src/variables',
+      '@providers': '/src/providers',
+    },
+  },
   plugins: [react()],
 })
