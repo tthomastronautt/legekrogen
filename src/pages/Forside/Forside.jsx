508485bf3d4d4a3ac95d1fc86eb2912eb1f63956
tthomastronautt <thom8b38@mcdmstudent.dk>
2024-08-12 13:58:34 +0200

    resolve conflicts

--- src/pages/Forside/Forside.jsx
+++ src/pages/Forside/Forside.jsx
@@ -1,6 +1,8 @@
 import Welcome from "@components/Welcome/Welcome";
-import forsidenImg from "@assets/heros/forsiden.jpg";
-import { welcomeTitle } from "@variables/welcome-title.js";
+import { welcomeData } from "@variables/welcomeData.js";
+import PronouncesSection from "@components/PronouncesSection/PronouncesSection.jsx"
+import BecomeCustomer from "@components/BecomeCustomer/BecomeCustomer.jsx"
+import ToyShowcase from "@components/ToyShowcase/ToyShowcase";
 
 const Forside = () => {
     return (
@@ -9,9 +11,9 @@ const Forside = () => {
                 {...welcomeData.forside}
                 titleType="h1"
             />
+            <ToyShowcase />
             <PronouncesSection />
             <BecomeCustomer/>
-            <ToyShowcase />
         </>
     );
 };
