316f8eb93148f86a5843ab3b78e5784ea95a42fa
tthomastronautt <thom8b38@mcdmstudent.dk>
2024-08-12 11:00:47 +0200

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,19 @@
+import { FaFacebook, FaInstagram, FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 import styles from "./footer.module.css";
+import Title from "@components/Title/Title.jsx"
 
 const Footer = () => {
     return(
         <div className={styles.container}>
-            <h1 className={styles.title}>Kundeservice</h1>
-            <p className={styles.info}>kontakt@legehjørnet.dk</p>
-            <p className={styles.info}>+45 23 45 89</p>
-            <h1 className={styles.title}>Følg os</h1>
+            <Title type="h2" className={styles.title}>Kundeservice</Title>
+            <p className={styles.info}><MdOutlineEmail />kontakt@legehjørnet.dk</p>
+            <p className={styles.info}><FaPhoneAlt /> +45 23 45 89</p>
+            <Title type="h2" className={styles.title}>Følg os</Title>
+            <Title type="h1" className={styles.social}>
+                <FaFacebook />
+                <FaInstagram />
+            </Title>
         </div>
     )
 }
